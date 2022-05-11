a07ed3b9baf55fa941be9f7c610eecccb880ef5f
ckost9n <ckost9n@gmail.com>
2022-05-11 21:05:08 +0300

    add delete method in comment

--- ToDo Grad/Controllers/ToDoListViewController.swift
+++ ToDo Grad/Controllers/ToDoListViewController.swift
@@ -110,7 +110,11 @@ extension ToDoListViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
         itemArray[indexPath.row].done.toggle()
+        
         saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
